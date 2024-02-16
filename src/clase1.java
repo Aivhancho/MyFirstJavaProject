6658a613e90f0f2b3dae751c6384b47e846df545
ivan <falso@falso.es>
2024-02-16 09:37:37 +0100

    Nueva clase clase1 con atributo

---
+++ src/clase1.java
@@ -0,0 +1,8 @@
+public class clase1 {
+    public static void main(String[] args) {
+        int num = 5;
+        if (num == 5){
+            System.out.println("Por el culo te la hinco");
+        }
+    }
+}
